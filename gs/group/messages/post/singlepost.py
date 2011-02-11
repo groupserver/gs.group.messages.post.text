0ff3bb942d54b08a3a73b345ae0857b5e002ddef
Michael JasonSmith <mpj17@onlinegroups.net>
2011-02-11 16:04:09 +1100

    Throwing 404 errors, and removing old methods

--- gs/group/messages/post/singlepost.py
+++ gs/group/messages/post/singlepost.py
@@ -1,15 +1,19 @@
 # coding=utf-8
+from zope.publisher.interfaces import NotFound
 from zope.security.interfaces import Unauthorized
 from Products.XWFMailingListManager.queries import MessageQuery
 from Products.GSGroup.utils import is_public
 from gs.group.base.page import GroupPage
+from error import NoIDError
 
 class GSPostView(GroupPage):
     def __init__(self, context, request):
         GroupPage.__init__(self, context, request)
         self.__isPublic = self.__messageQuery = self.__post = \
             self.__relatedPosts = None
-        self.postId = self.request['postId']
+        self.postId = self.request.get('postId', None)
+        if not self.postId:
+            raise NoIDError('No ID Specified')
     
     @property
     def isPublic(self):
@@ -25,17 +29,23 @@ class GSPostView(GroupPage):
             da = self.context.zsqlalchemy 
             assert da, 'No data-adaptor found'
             self.__messageQuery = MessageQuery(self.context, da)
+        assert self.__messageQuery
         return self.__messageQuery
 
     @property
     def post(self):
         if self.__post == None:
             self.__post = self.messageQuery.post(self.postId)
-            assert self.__post # TODO: Raise something: Not found error
+            if not self.__post:
+              raise NotFound(self, self.postId, self.request)
             if self.__post['group_id'] != self.groupInfo.id:
                 m = u'You are not authorized to access this post from '\
                     'the group %s' % self.groupInfo.name
                 raise Unauthorized(m)
+            # TODO: Check for a post being hidden. Hidden posts will 
+            # sometimes raise a 410
+            # <https://projects.iopen.net/groupserver/ticket/316>
+        assert self.__post
         return self.__post
         
     @property
@@ -45,19 +55,8 @@ class GSPostView(GroupPage):
                 self.messageQuery.topic_post_navigation(self.postId)
         return self.__relatedPosts
 
-    def do_error_redirect(self):
-        if not self.postId:
-            self.request.response.redirect('/r/post-no-id')
-        else:
-            self.request.response.redirect('/r/post-not-found?id=%s' % self.postId)
-    
     @property
     def topicTitle(self):
-        assert hasattr(self, 'post')
         retval = self.post.get('subject', '')
         return retval
-          
-    def get_post(self):
-        assert hasattr(self, 'post')
-        return self.post
 
