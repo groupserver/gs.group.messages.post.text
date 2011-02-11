337efae09a6c8aecefef5a49f8e0c531b8334046
Michael JasonSmith <mpj17@onlinegroups.net>
2011-02-11 16:23:42 +1100

    Adding logging for 410 and 500 errors

--- gs/group/messages/post/traverse.py
+++ gs/group/messages/post/traverse.py
@@ -4,6 +4,11 @@ from zope.component import getMultiAdapter
 from zope.location.interfaces import LocationError
 from zope.publisher.interfaces import NotFound
 from gs.group.base.page import GroupPage
+from error import NoIDError
+
+SUBSYSTEM = 'gs.group.messages.post'
+import logging
+log = logging.getLogger(SUBSYSTEM) #@UndefinedVariable
 
 class GSPostTraversal(GroupPage):
     def __init__(self, context, request):
@@ -26,9 +31,16 @@ class GSPostTraversal(GroupPage):
             self.request.form['r'] = self.request.get('HTTP_REFERER','')
             retval = getMultiAdapter((self.context, self.request),
                         name="new_not_found.html")()
+        except NoIDError, n:
+            uri = '%s/messages/topics.html' % self.groupInfo.url
+            m = 'No post ID in <%s>. Going to <%s>' % \
+                (self.request.URL, uri)
+            log.info(m)
+            retval = self.request.RESPONSE.redirect(uri)
         except Exception, e:
             self.request.form['q'] = self.request.URL
             self.request.form['m'] = format_exc()
+            log.error(format_exc())
             retval = getMultiAdapter((self.context, self.request),
                         name="new_unexpected_error.html")()
         return retval
