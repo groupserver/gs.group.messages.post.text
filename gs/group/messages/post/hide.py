23343308ac9df4b458759a61a66f363c004bfab8
Michael JasonSmith <mpj17@onlinegroups.net>
2011-04-13 15:43:16 +1000

    Actually hide

--- gs/group/messages/post/hide.py
+++ gs/group/messages/post/hide.py
@@ -1,10 +1,13 @@
 # coding=utf-8
 from zope.formlib import form
+from zope.cachedescriptors.property import Lazy
+from zope.component import createObject
 from zope.i18nmessageid import MessageFactory
 _ = MessageFactory('groupserver')
 from Products.Five.browser.pagetemplatefile import ZopeTwoPageTemplateFile
 from gs.group.base.form import GroupForm
 from interfaces import IHide
+from queries import PostQuery
 
 class HidePost(GroupForm):
     form_fields = form.Fields(IHide)
@@ -17,13 +20,31 @@ class HidePost(GroupForm):
 
     @form.action(label=_('Hide'), failure='handle_failure')
     def handle_hide(self, action, data):
-        self.status = u''
+        self.postQuery.hide_post(data['postId'], self.loggedInUser.id, 
+                                    data['reason'])
+        self.status = u'Hidden the post.'
         uri = '/r/topic/%s' % data['postId']
         self.request.RESPONSE.redirect(uri)
     
     def handle_failure(self, action, data, errors):
         if len(errors) == 1:
-            self.status = u'<p>There is an error:</p>'
+            self.status = _(u'<p>There is an error:</p>')
         else:
-            self.status = u'<p>There are errors:</p>'
+            self.status = _(u'<p>There are errors:</p>')
+
+    @Lazy
+    def postQuery(self):
+        assert self.context
+        da = self.context.zsqlalchemy
+        assert da
+        retval = PostQuery(self.context, da)
+        assert retval
+        return retval
+
+    @Lazy
+    def loggedInUser(self):
+        retval = createObject('groupserver.LoggedInUser', self.context)
+        assert retval
+        assert not(retval.anonymous), 'Anon trying to hide a post!'
+        return retval
 
--- gs/group/messages/post/queries.py
+++ gs/group/messages/post/queries.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 import sqlalchemy as sa
-import datetime
+from datetime import datetime
 from pytz import UTC
 
 class PostQuery(object):
@@ -24,4 +24,20 @@ class PostQuery(object):
                 'hiding_user':  row['hiding_user'],
                 'reason':       row['reason']}
         return retval
+
+    def hide_post(self, postId, userId, reason):
+        now = datetime.now(UTC)
+        self.update_post_table(postId, now)
+        self.update_hidden_post_table(postId, now, userId, reason)
+        
+    def update_post_table(self, postId, dt):
+        u = self.postTable.update(self.postTable.c.post_id == postId)
+        u.execute(hidden = dt)
+    
+    def update_hidden_post_table(self, postId, dt, userId, reason):
+        i = self.hiddenPostTable.insert()
+        i.execute(post_id = postId, 
+            date_hidden = dt,
+            hiding_user = userId,
+            reason = reason)
 
