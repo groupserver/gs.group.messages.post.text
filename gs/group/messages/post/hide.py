69d8fd06563177390bffd91260e87f813aa52819
Michael JasonSmith <mpj17@onlinegroups.net>
2011-04-15 15:38:26 +1000

    Checking if the user can hide the post in the Hide code itself.

--- gs/group/messages/post/hide.py
+++ gs/group/messages/post/hide.py
@@ -1,13 +1,17 @@
 # coding=utf-8
+from zope.publisher.interfaces import NotFound
 from zope.formlib import form
 from zope.cachedescriptors.property import Lazy
 from zope.component import createObject
+from zope.security.interfaces import Unauthorized
 from zope.i18nmessageid import MessageFactory
 _ = MessageFactory('groupserver')
 from Products.Five.browser.pagetemplatefile import ZopeTwoPageTemplateFile
+from Products.XWFMailingListManager.queries import MessageQuery
 from gs.group.base.form import GroupForm
 from interfaces import IHide
 from queries import PostQuery
+from canhide import can_hide_post
 
 class HidePost(GroupForm):
     form_fields = form.Fields(IHide)
@@ -20,6 +24,12 @@ class HidePost(GroupForm):
 
     @form.action(label=_('Hide'), failure='handle_failure')
     def handle_hide(self, action, data):
+        postInfo = self.get_post(data['postId'])
+        if not(can_hide_post(self.loggedInUser, self.groupInfo, postInfo)):
+            # Do not try and hack the URL, bastard.
+            m = 'You are not allowed to hide the post %s in %s (%s)' %\
+                (data['postId'], self.groupInfo.name, self.groupInfo.id)
+            raise Unauthorized(m)
         self.postQuery.hide_post(data['postId'], self.loggedInUser.id, 
                                     data['reason'])
         self.status = u'Hidden the post.'
@@ -46,5 +56,25 @@ class HidePost(GroupForm):
         retval = createObject('groupserver.LoggedInUser', self.context)
         assert retval
         assert not(retval.anonymous), 'Anon trying to hide a post!'
+        return retval
+
+    @Lazy
+    def messageQuery(self):
+        assert self.context, 'No context for a post!'
+        da = self.context.zsqlalchemy 
+        assert da, 'No data-adaptor found'
+        retval = MessageQuery(self.context, da)
+        assert retval
+        return retval
+
+    def get_post(self, postId):
+        retval = self.messageQuery.post(postId)
+        if not retval:
+          raise NotFound(self, postId, self.request)
+        if retval['group_id'] != self.groupInfo.id:
+            m = u'You are not authorized to access this post from '\
+                'the group %s' % self.groupInfo.name
+            raise Unauthorized(m)
+        assert retval
         return retval
 
