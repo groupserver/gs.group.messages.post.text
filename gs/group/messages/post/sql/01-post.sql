7648f63aa9d276df7a6a436b4979c157e03502bf
Michael JasonSmith <mpj17@onlinegroups.net>
2011-04-29 14:59:19 +1000

    Adding the count_rows table creation

--- gs/group/messages/post/sql/01-post.sql
+++ gs/group/messages/post/sql/01-post.sql
@@ -23,6 +23,32 @@ CREATE TABLE post (
 CREATE INDEX site_group_idx ON post USING BTREE (site_id, group_id);
 CREATE INDEX topic_idx ON post USING BTREE (topic_id);
 
+-- Create the row-count table, which is used all over the place
+-- TODO: Move to a base product. Maybe create a gs.base or put it in
+-- gs.group.messages.base?
+CREATE TABLE rowcount (
+    table_name  text NOT NULL,
+    total_rows  bigint,
+    PRIMARY KEY (table_name)
+);
+
+CREATE OR REPLACE FUNCTION count_rows()
+RETURNS TRIGGER AS
+'
+   BEGIN
+      IF TG_OP = ''INSERT'' THEN
+         UPDATE rowcount
+            SET total_rows = total_rows + 1
+            WHERE table_name = TG_RELNAME;
+      ELSIF TG_OP = ''DELETE'' THEN
+         UPDATE rowcount
+            SET total_rows = total_rows - 1
+            WHERE table_name = TG_RELNAME;
+      END IF;
+      RETURN NULL;
+   END;
+' LANGUAGE plpgsql;
+
 -- Initialise the trigger and rowcount for the post table
 BEGIN;
    -- Make sure no rows can be added to post until we have finished
