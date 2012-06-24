d19ac7e50404fce3ed553937e60d714a7fffa8a8
richard@onlinegroups.net <richard@onlinegroups.net>
2012-06-24 17:58:41 +1200

    New error messages no longer require redirection hack

--- gs/group/messages/post/traverse.py
+++ gs/group/messages/post/traverse.py
@@ -2,7 +2,6 @@
 from traceback import format_exc
 from zope.component import getMultiAdapter
 from zope.location.interfaces import LocationError
-from zope.publisher.interfaces import NotFound
 from gs.group.base.page import GroupPage
 from error import NoIDError, Hidden
 
@@ -25,22 +24,11 @@ class GSPostTraversal(GroupPage):
         # <https://projects.iopen.net/groupserver/ticket/316>
         try:
             retval = getMultiAdapter((self.context, self.request), name="gspost")()
-        except NotFound, n:
-            self.request.form['q'] = self.request.URL
-            self.request.form['r'] = self.request.get('HTTP_REFERER','')
-            retval = getMultiAdapter((self.context, self.request),
-                        name="new_not_found.html")()
         except NoIDError, n:
             uri = '%s/messages/topics.html' % self.groupInfo.url
             m = 'No post ID in <%s>. Going to <%s>' % \
                 (self.request.URL, uri)
             log.info(m)
             retval = self.request.RESPONSE.redirect(uri)
-        except Exception, e:
-            self.request.form['q'] = self.request.URL
-            self.request.form['m'] = format_exc()
-            log.error(format_exc())
-            retval = getMultiAdapter((self.context, self.request),
-                        name="new_unexpected_error.html")()
         return retval
 
