fe4d16c83d94a7b1d0563003bf7848896da092e3
Michael JasonSmith <mpj17@onlinegroups.net>
2012-12-12 14:41:54 +1100

    Switching to jQuery UI for Hide a Post.

--- gs/group/messages/post/browser/javascript/hide-post.js
+++ gs/group/messages/post/browser/javascript/hide-post.js
@@ -1,35 +1,45 @@
 jQuery.noConflict();
 GSHidePost = function () {
-  var showDialog = function(event) {
-    var style  ='';
-    var post = null;
-    var pos = null;
-    var postId = '';
-    
-    // Get the position of the post (hentry) that contains the
-    // hide button that has been clicked.
-    post = jQuery(this).parents('.hentry');
-    postId = post.attr('id');
-    postId = postId.slice(5);
-                                                                                                 
-    pos = post.position();
-    // Position the Hide UI over the post.
-    style = style + 'top:'+ pos.top+'px;display:block;';
-    jQuery('#hide-the-post')
-      .attr('style', style);
-    jQuery("#hide-the-post").load('../hide_post.ajax', {'form.postId': postId});
-  };
+    var hideButtons = jQuery('.hide-button');
+    var dialog = jQuery('#hide-the-post');
+    var loading = jQuery('#hide-the-post .loading');
+
+    var showDialog = function(event) {
+        var style  ='';
+        var post = null;
+        var pos = null;
+        var postId = '';
+        
+        // Get the position of the post (hentry) that contains the
+        // hide button that has been clicked.
+        post = jQuery(this).parents('.hentry');
+        postId = post.attr('id');
+        postId = postId.slice(5);
+        loading.load('../hide_post.ajax', {'form.postId': postId}, loaded);
+
+        // TODO: Position http://api.jqueryui.com/position/
+        dialog.dialog("open");
+    };
+
+    var loaded = function(response, status, request) {
+        var icons = {primary: 'ui-icon-trash'};
+        jQuery('#form\\.actions\\.hide').button({icons: icons, text: true});
+    };
   
-  return {
-    init: function () {
-      jQuery('.hide-button')
-        .removeAttr('href')
-        .click(showDialog);
-      }
-  };
+    return {
+        init: function () {
+            var hide = {icons: {primary: 'ui-icon-trash'}};
+
+            hideButtons.removeAttr('href').click(showDialog).button(hide);
+
+            
+            dialog.dialog({autoOpen: false, closeOnEscape: true, modal: true,
+                           resizable: true, title: "Hide Post",
+                           minWidth: 504}); // 28u
+        }
+    };
 }(); // GSHidePost
 
 jQuery(document).ready( function () {
-  GSHidePost.init()
+    GSHidePost.init()
 });
-
