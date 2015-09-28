4ef450c7c522ded6f3f1b4c445e0c4a8acec6876
Michael JasonSmith <mpj17@onlinegroups.net>
2015-09-28 16:01:45 -0700

    Tweaking the meatadata

--- setup.py
+++ setup.py
@@ -19,6 +19,7 @@ import os
 from setuptools import setup, find_packages
 from version import get_version
 
+name = 'gs.group.messages.post'
 version = get_version()
 
 with codecs.open('README.rst', encoding='utf-8') as f:
@@ -28,7 +29,7 @@ with codecs.open(os.path.join("docs", "HISTORY.rst"),
     long_description += '\n' + f.read()
 
 setup(
-    name='gs.group.messages.post',
+    name=name,
     version=version,
     description="Posts made to a GroupServer Group",
     long_description=long_description,
@@ -49,10 +50,11 @@ setup(
     author_email='richard@onlinegroups.net',
     maintainer='Michael JasonSmith',
     maintainer_email='mpj17@onlinegroups.net',
-    url='https://github.com/groupserver/gs.group.messages.post/',
+    url='https://github.com/groupserver/{0}/'.format(name),
     license='ZPL 2.1',
     packages=find_packages(exclude=['ez_setup']),
-    namespace_packages=['gs', 'gs.group', 'gs.group.messages'],
+    namespace_packages=['.'.join(name.split('.')[:i])
+                        for i in range(1, len(name.split('.')))],
     include_package_data=True,
     zip_safe=False,
     install_requires=[
