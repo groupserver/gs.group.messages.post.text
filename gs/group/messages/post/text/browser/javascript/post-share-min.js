b3fe0ca6c6521e4c6fd1f66342f8f19142455df4
Michael JasonSmith <mpj17@onlinegroups.net>
2015-09-30 09:26:44 -0700

    Removing the core code

--- gs/group/messages/post/text/browser/javascript/post-share-min.js
+++
@@ -1,4 +0,0 @@
-"use strict";jQuery.noConflict();function gs_group_messages_post_share_init(){var b=false,a=null;
-jQuery(".gs-content-js-share").each(function(){b=Boolean(Number(jQuery(this).attr("public")));
-a=GSShareBox(this,b);a.init()})}jQuery(window).load(function(){gsJsLoader.with_module("/++resource++gs-content-js-sharebox-min-20140327.js",gs_group_messages_post_share_init)
-});
--- gs/group/messages/post/text/interfaces.py
+++ gs/group/messages/post/text/interfaces.py
@@ -12,82 +12,8 @@
 # FOR A PARTICULAR PURPOSE.
 #
 ############################################################################
-from __future__ import absolute_import, unicode_literals
+from __future__ import absolute_import, unicode_literals, print_function
 from zope.interface import Interface
-from zope.schema import TextLine, ASCIILine, Field, Int, Bool
-from gs.core import to_ascii
-
-
-class INavLinksContentProvider(Interface):
-    topicTitle = TextLine(
-        title="Title of the Topic",
-        description='The title of the topic.',
-        required=True)
-
-    relatedPosts = Field(
-        title='Related Posts',
-        description='The posts in the same topic.',
-        required=True)
-
-    pageTemplateFileName = ASCIILine(
-        title="Page Template File Name",
-        description="""The name of the ZPT file
-        that is used to render the post.""",
-        required=False,
-        default=to_ascii('browser/templates/navlinks.pt'))
-
-
-class IGSPostContentProvider(Interface):
-    """The Groupserver Post Content Provider Interface
-
-      This interface defines the fields that must be set up, normally using
-      TAL, before creating a "GSPostContentProvider" instance. See the
-      latter for an example."""
-    post = Field(
-        title="Email Message Instance",
-        description="The email instance to display",
-        required=True,
-        readonly=False)
-    position = Int(
-        title="Position of the Post",
-        description="""The position of the post in the topic.
-        This is mostly used for determining the background
-        colour of the post.""",
-        required=False,
-        min=1, default=1)
-    topicName = TextLine(
-        title="Title of the Topic",
-        description="""The title of the topic.""",
-        required=True,
-        default='')
-    # Should really be called "same author" or similar.
-    showPhoto = Bool(
-        title='Whether to show the photo',
-        description="""Determines if the author's photo
-        should be shown.""",
-        required=False,
-        default=True)
-
-    isPublic = Bool(
-        title="Is the group public?",
-        description="""Whether or not the group in which this post is
-          displayed is public""",
-        required=True)
-
-    showRemainder = Bool(
-        title='Show the remainder',
-        description='True if the bottom-quoting should be shown',
-        required=False,
-        default=False)
-
-    pageTemplateFileName = ASCIILine(
-        title="Page Template File Name",
-        description='The name of the ZPT file that is used to render '
-                    'the post.',
-        required=False,
-        default=to_ascii("browser/templates/postcontentprovider.pt"))
-
-# Used for some utilities
 
 
 class IMarkupEmail(Interface):
