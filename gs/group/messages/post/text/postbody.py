b1c4064623e46e94abe8bd5510341d1a65fe7d57
Michael JasonSmith <mpj17@onlinegroups.net>
2015-10-20 17:22:52 -0700

    Minor speed improvement

--- gs/group/messages/post/text/postbody.py
+++ gs/group/messages/post/text/postbody.py
@@ -279,17 +279,17 @@ Originally a stand-alone script in ``Presentation/Tofu/MailingListManager/lscrip
         if len(intro) < 5:
             trim = False
 
-        ls = line[:4] if len(line) > 3 else ''
         if trim:
-                # IF we are trimming, and we are looking at a quote-character or an empty string
-                #   OR we have seen the 'wrote:' marker,
-                #   OR the line has non-whitepsace characters AND there is only one word on the
-                #   line, AND the previous line does NOT have any significant text
-                # THEN add it to the snipped-text.
-            if (((ls == '&gt;') or (ls[:2] == '> ') or (ls.strip() == ''))
-                    or (line.find('wrote:') > 2)
-                    or ((len(line.strip()) > 0) and (len(line.strip().split()) == 1)
-                        and ((len(prevLine.strip()) == 0) or len(prevLine.strip().split()) == 1))):
+            ls = line[:4] if len(line) > 3 else ''
+            # IF we are trimming, and we are looking at a quote-character or an empty string
+            #   OR we have seen the 'wrote:' marker,
+            #   OR the line has non-whitepsace characters AND there is only one word on the
+            #   line, AND the previous line does NOT have any significant text
+            # THEN add it to the snipped-text.
+            if (((ls == '&gt;') or (ls[:2] == '> ') or (ls.strip() == '')
+                 or (line.find('wrote:') > 2))
+                or ((len(line.strip()) > 0) and (len(line.strip().split()) == 1)
+                    and ((len(prevLine.strip()) == 0) or len(prevLine.strip().split()) == 1))):
                 remainder.insert(0, line)
             else:
                 trim = False
