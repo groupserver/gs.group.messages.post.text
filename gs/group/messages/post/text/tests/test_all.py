4bdce6078137235cbe5d16920e47b1c5420f33d0
Michael JasonSmith <mpj17@onlinegroups.net>
2015-10-29 10:59:18 -0700

    Moving the split-message code into its own module.

--- gs/group/messages/post/text/tests/test_all.py
+++ gs/group/messages/post/text/tests/test_all.py
@@ -14,7 +14,8 @@
 ############################################################################
 from __future__ import absolute_import, unicode_literals
 from unittest import TestSuite, main as unittest_main
-from gs.group.messages.post.text.tests.postbody import (YouTubeTest, VimeoTest, SplitMessageTest, )
+from gs.group.messages.post.text.tests.postbody import (YouTubeTest, VimeoTest, )
+from gs.group.messages.post.text.tests.splitmessage import (SplitMessageTest, )
 from gs.group.messages.post.text.tests.viewlet import (TestTextPostViewlet, )
 testCases = (YouTubeTest, VimeoTest, TestTextPostViewlet, SplitMessageTest, )
 
